e0fbfd82e99c65d7ca74f4fbc2b8f14831069936
changes <gowthamisundharaj@gmail.com>
2022-09-06 11:30:47 -0500

    surfire plugin version

--- src/test/java/SeleniumBasic.java
+++ src/test/java/SeleniumBasic.java
@@ -1,4 +1,6 @@
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;   //used to validate response status
 import org.testng.annotations.Test;
 
@@ -16,6 +18,8 @@ WebDriver driver;
         driver=new ChromeDriver();
         driver.get("https://www.google.com");
         String t=driver.getTitle();
+        WebElement el= driver.findElement(By.name("q"));
+        el.sendKeys("shapes");
         System.out.println(t);
         Assert.assertEquals(t,"Google");
         driver.close();
