5c9579fb6e6a887f98e700bacab34753c21a8cf2
changes <gowthamisundharaj@gmail.com>
2022-09-06 12:06:16 -0500

    surfire plugin version

--- src/test/java/SeleniumBasic.java
+++ src/test/java/SeleniumBasic.java
@@ -2,6 +2,8 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;   //used to validate response status
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -9,19 +11,39 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class SeleniumBasic {
 WebDriver driver;
+   @BeforeTest
+   public void test1()
+   {
+       WebDriverManager.chromedriver().setup();
+       driver=new ChromeDriver();
+       driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS) ;
+
+       String baseUrl = "http://demo.guru99.com/test/login.html";
+       driver.get(baseUrl);
+       driver.manage().window().maximize() ;
+   }
     @Test
-    public void test()
+    public void test2()
     {
-        WebDriverManager.chromedriver().setup();
-        driver=new ChromeDriver();
-        driver.get("https://www.google.com");
-        String t=driver.getTitle();
-        WebElement el= driver.findElement(By.name("q"));
-        el.sendKeys("shapes");
-        System.out.println(t);
-        Assert.assertEquals(t,"Google");
-        driver.close();
+        WebElement email = driver.findElement(By.id("email"));
+         WebElement password = driver.findElement(By.name("passwd"));
+         email.sendKeys("abcd@gmail.com");
+        password.sendKeys("abcdefghlkjl");
+
+        WebElement login = driver.findElement(By.id("SubmitLogin"));
+        login.click();
+
     }
+    @AfterTest
+    public void test3()
+    {
+try {
+    Thread.sleep(20000);
+    driver.close();
+}
+    catch (InterruptedException e){}}
 }
