54344ddfedbe5b25085e73b9da5bbdcc260c0989
changes <gowthamisundharaj@gmail.com>
2022-09-08 09:49:50 -0500

    surfire plugin version

---
+++ src/test/java/ParameterByDataprovider.java
@@ -0,0 +1,51 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class ParameterByDataprovider {
+    WebDriver driver;
+    String driverPath = "C:\\geckodriver.exe";
+
+    @BeforeTest
+    public void setup(){
+       WebDriverManager.chromedriver().setup();
+       driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://google.com");
+    }
+
+    @Test(dataProvider="SearchProvider")
+    public void testMethod(String author,String searchKey) throws InterruptedException{
+        {
+            WebElement searchText = driver.findElement(By.name("q"));
+            searchText.sendKeys(searchKey);
+            System.out.println("Welcome ->"+author+" Your search key is->"+searchKey);
+            Thread.sleep(3000);
+            String testValue = searchText.getAttribute("value");
+            System.out.println(testValue +"::::"+searchKey);
+            searchText.clear();
+            //Verify if the value in google search box is correct
+            Assert.assertTrue(testValue.equalsIgnoreCase(searchKey));
+            driver.close();
+        }
+    }
+
+    @DataProvider(name="SearchProvider")
+    public Object[][] getDataFromDataprovider(){
+        return new Object[][]
+                {
+                        { "Guru99", "India" },
+                        { "Krishna", "UK" },
+                        { "Bhupesh", "USA" }
+                };
+
+    }
+}
